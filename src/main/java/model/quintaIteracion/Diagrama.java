d019b572dc5ce8e65bd7e0b97403703273c6db39
AlejandroDeheza <ale.deheza@gmail.com>
2020-06-05 23:46:09 -0300

    Actualizo diagrama

--- src/main/java/model/quintaIteracion/Diagrama.java
+++ src/main/java/model/quintaIteracion/Diagrama.java
@@ -6,60 +6,7 @@ public class Diagrama {
 
 /*
 @startuml
-class Guardarropa [[java:model.paraDiagrama.Guardarropa]] {
-	-String criterioDeUsuario
-	+Guardarropa(String criterioDeUsuario)
-	+void agregarPrenda(Prenda prenda)
-	+void eliminarPrenda(Prenda prenda)
-	+List<Atuendo> todasLasPosiblesCombinaciones()
-}
-class Prenda [[java:model.Prenda]] {
-}
-Guardarropa --> "*" Prenda : prendas
-interface Propuesta [[java:model.paraDiagrama.Propuesta]] {
-	void aplicar()
-	void deshacer()
-}
-class Prenda [[java:model.paraDiagrama.Prenda]] {
-	-TipoPrenda tipoPrenda
-	-Material material
-	-Color colorPrimario
-	-Color colorSecundario
-	-Trama trama
-	-int temperaturaMaxima
-	+Prenda(TipoPrenda tipoPrenda, Material material, Color colorPrimario, Color colorSecundario, Trama trama)
-	+Categoria getCategoria()
-	+boolean aptaParaTemperatura(int temperatura)
-}
-class PropuestaEliminarPrenda [[java:model.paraDiagrama.PropuestaEliminarPrenda]] {
-	+PropuestaEliminarPrenda(Prenda prenda, Guardarropa guardarropa)
-	+void aplicar()
-	+void deshacer()
-}
-class Prenda [[java:model.Prenda]] {
-}
-PropuestaEliminarPrenda --> "1" Prenda : prenda
-class Guardarropa [[java:model.quintaIteracion.Guardarropa]] {
-}
-PropuestaEliminarPrenda --> "1" Guardarropa : guardarropa
-interface Propuesta [[java:model.paraDiagrama.Propuesta]] {
-}
-Propuesta <|.. PropuestaEliminarPrenda
-class PropuestaAgregarPrenda [[java:model.paraDiagrama.PropuestaAgregarPrenda]] {
-	+PropuestaAgregarPrenda(Prenda prenda, Guardarropa guardarropa)
-	+void aplicar()
-	+void deshacer()
-}
-class Prenda [[java:model.Prenda]] {
-}
-PropuestaAgregarPrenda --> "1" Prenda : prenda
-class Guardarropa [[java:model.quintaIteracion.Guardarropa]] {
-}
-PropuestaAgregarPrenda --> "1" Guardarropa : guardarropa
-interface Propuesta [[java:model.paraDiagrama.Propuesta]] {
-}
-Propuesta <|.. PropuestaAgregarPrenda
-class Usuario [[java:model.paraDiagrama.Usuario]] {
+class Usuario [[java:model.quintaIteracion.Usuario]] {
 	+void compartirGuardarropa(Guardarropa guardarropa)
 	+void aceptarTodasLasPropuestas()
 	+void rechazarTodasLasPropuestas()
@@ -71,12 +18,64 @@ class Usuario [[java:model.paraDiagrama.Usuario]] {
 class Guardarropa [[java:model.quintaIteracion.Guardarropa]] {
 }
 Usuario --> "*" Guardarropa : guardarropas
-class Propuesta [[java:model.paraDiagrama.Propuesta]] {
+class Propuesta [[java:model.quintaIteracion.Propuesta]] {
 }
 Usuario --> "*" Propuesta : propuestas
-class Propuesta [[java:model.paraDiagrama.Propuesta]] {
+class Propuesta [[java:model.quintaIteracion.Propuesta]] {
 }
 Usuario --> "*" Propuesta : propuestasAceptadas
+class PropuestaAgregarPrenda [[java:model.quintaIteracion.PropuestaAgregarPrenda]] {
+	+PropuestaAgregarPrenda(Prenda prenda, Guardarropa guardarropa)
+	+void aplicar()
+	+void deshacer()
+}
+class Prenda [[java:model.prenda.Prenda]] {
+}
+PropuestaAgregarPrenda --> "1" Prenda : prenda
+class Guardarropa [[java:model.quintaIteracion.Guardarropa]] {
+}
+PropuestaAgregarPrenda --> "1" Guardarropa : guardarropa
+interface Propuesta [[java:model.quintaIteracion.Propuesta]] {
+}
+Propuesta <|.. PropuestaAgregarPrenda
+interface Propuesta [[java:model.quintaIteracion.Propuesta]] {
+	void aplicar()
+	void deshacer()
+}
+class PropuestaEliminarPrenda [[java:model.quintaIteracion.PropuestaEliminarPrenda]] {
+	+PropuestaEliminarPrenda(Prenda prenda, Guardarropa guardarropa)
+	+void aplicar()
+	+void deshacer()
+}
+class Prenda [[java:model.prenda.Prenda]] {
+}
+PropuestaEliminarPrenda --> "1" Prenda : prenda
+class Guardarropa [[java:model.quintaIteracion.Guardarropa]] {
+}
+PropuestaEliminarPrenda --> "1" Guardarropa : guardarropa
+interface Propuesta [[java:model.quintaIteracion.Propuesta]] {
+}
+Propuesta <|.. PropuestaEliminarPrenda
+class Guardarropa [[java:model.quintaIteracion.Guardarropa]] {
+	+Guardarropa()
+	+void agregarPrenda(Prenda prenda)
+	+void eliminarPrenda(Prenda prenda)
+	+List<Atuendo> todasLasPosiblesCombinaciones()
+}
+class Prenda [[java:model.prenda.Prenda]] {
+}
+Guardarropa --> "*" Prenda : prendas
+class Prenda [[java:model.quintaIteracion.Prenda]] {
+	-TipoPrenda tipoPrenda
+	-Material material
+	-Color colorPrimario
+	-Color colorSecundario
+	-Trama trama
+	-int temperaturaMaxima
+	+Prenda(TipoPrenda tipoPrenda, Material material, Color colorPrimario, Color colorSecundario, Trama trama)
+	+Categoria getCategoria()
+	+boolean aptaParaTemperatura(int temperatura)
+}
 @enduml
 
 */
